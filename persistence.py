135b7cbe1454e1c872fbc71b172b324aa22c2717
karis courey <kariscourey@gmail.com>
2022-09-09 20:49:25 -0400

    update pers

--- persistence.py
+++ persistence.py
@@ -1,31 +1,41 @@
+# def persistence(n):
+
+
+#     def product_calc(foo):
+
+#         product = 1
+
+#         for i in str(foo):
+#             product *= int(i)
+
+#         return product
+
+#     count = 0
+
+#     while n >= 10:
+
+#         n = product_calc(n)
+#         count += 1
+
+#     return count
+
+
+import operator
 def persistence(n):
-
-
-    def product_calc(foo):
-
-        product = 1
-
-        for i in str(foo):
-            product *= int(i)
-
-        return product
-
-    count = 0
-
-    while n > 10:
-
-        n = product_calc(n)
-        count += 1
-
-    return count
-
+    i = 0
+    while n>=10:
+        n=reduce(operator.mul,[int(x) for x in str(n)],1)
+        i+=1
+    return i
 
 # print(persistence(39))
 # print(persistence(999))
 # print(persistence(4))
-print(persistence(1))
-print(persistence(38791))
-print(persistence(416719))
+# print(persistence(1))
+# print(persistence(38791))
+# print(persistence(416719))
+print(persistence(25))
+
 
 
 # 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
